c95288f2cbb6e9432673a62dc72f81a4ce3ea8c1
talhadengiz <talhadengiz@gmail.com>
2021-11-27 01:25:43 +0300

    ViewBinding added.

--- app/src/main/java/com/talhadengiz/tomurcuk/ui/fragment/requirement/AddRequirementFragment.kt
+++ app/src/main/java/com/talhadengiz/tomurcuk/ui/fragment/requirement/AddRequirementFragment.kt
@@ -6,14 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.talhadengiz.tomurcuk.R
+import com.talhadengiz.tomurcuk.databinding.FragmentAddRequirementBinding
 
 class AddRequirementFragment : Fragment() {
+
+    private lateinit var binding : FragmentAddRequirementBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_requirement, container, false)
+        binding = FragmentAddRequirementBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
--- app/src/main/java/com/talhadengiz/tomurcuk/ui/fragment/requirement/RequirementListFragment.kt
+++ app/src/main/java/com/talhadengiz/tomurcuk/ui/fragment/requirement/RequirementListFragment.kt
@@ -6,14 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.talhadengiz.tomurcuk.R
+import com.talhadengiz.tomurcuk.databinding.FragmentRequirementListBinding
 
 class RequirementListFragment : Fragment() {
+
+    private lateinit var binding : FragmentRequirementListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_requirement_list, container, false)
+        binding = FragmentRequirementListBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        //TODO
     }
 }
